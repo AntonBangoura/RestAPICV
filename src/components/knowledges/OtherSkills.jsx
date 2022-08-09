a68d4b294df4fcd8c770902c3577e011e9729c97
Anton Bangoura <bangouranton@gmail.com>
2022-08-09 17:53:02 +0200

    final

---
+++ src/components/knowledges/OtherSkills.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OtherSkills() {
+  return (
+    <div>OtherSkills</div>
+  )
+}
+
+export default OtherSkills
--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Languages from '../components/knowledges/Languages'
 import Navbar from '../components/Navbar'
+import Experience from '../components/knowledges/Experience'
+import OtherSkills from '../components/knowledges/OtherSkills'
+import Hobbies from '../components/knowledges/Hobbies'
 
 const Knowledges = () => {
   return (
-    <div>
+    <div className='knowledges'>
       <Navbar/>
+      <div className='knowledgesContent'>
+      <Languages/>
+      <Experience/>
+      <OtherSkills/>
+      <Hobbies/>
+
+      </div>
     </div>
   )
 }
